c82d48ee5cb04d82c7b57e42cce712e5aaacb7ba
Ikko-T <skyblueboy32@gmail.com>
2020-04-25 12:36:01 +0900

    RubyQ15修正

--- task.rb
+++ task.rb
@@ -129,8 +129,8 @@ def q15
   data2 = { name: "yamada", hobby: "baseball", role: "normal" }
 
   #  以下に回答を記載
-  p data1.find {|key| key == "age"} ? "OK" : "NG"
-  p data2.find {|key| key == "age"} ? "OK" : "NG"
+  p data1.key?(:age) ? "OK" : "NG"
+  p data2.key?(:age) ? "OK" : "NG"
 end
   
 def q16
